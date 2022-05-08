958f14cd9d67350a27c129fe1a0f6d50369da7bb
Js assignments <edwinthomasg29@gmail.com>
2022-05-08 23:03:10 +0530

    commit

--- Templates/express.js
+++ Templates/express.js
@@ -3,6 +3,10 @@ var app = express()
 
 app.set('view engine','pug')
 app.get('/',(req,res)=>{
-    res.render('index')
+    let user = {
+        name:'edwin',
+        age:21
+    }
+    res.render('index',{user})
 })
 app.listen(8080)
